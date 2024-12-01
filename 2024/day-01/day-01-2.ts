793314df653f45dfeb89455f8a7e0eb7f848f985
rolcho <roland@cimem.hu>
2024-12-01 20:06:33 +0100

    d01 optimize

--- 2024/day-01/day-01-2.ts
+++ 2024/day-01/day-01-2.ts
@@ -3,20 +3,17 @@ import { readFileSync } from "fs";
 
 const input = readFileSync("./input.txt", "utf8").trim();
 const lines = input.split("\n");
-const left: number[] = [];
-const right: number[] = [];
 let similarity = 0;
-
-const map = new Map<number, number>();
+const frequencyMap: { [key: number]: number } = {};
 
 lines.forEach((line) => {
-  const [leftNumber, rightNumber] = line.split("  ").map(Number);
-  left.push(leftNumber);
-  map.set(rightNumber, (map.get(rightNumber) || 0) + 1);
+  const [_, right] = line.split("  ").map(Number);
+  frequencyMap[right] = frequencyMap[right] + 1 || 1;
 });
 
-for (let i = 0; i < left.length; i++) {
-  similarity += left[i] * (map.get(left[i]) ?? 0);
-}
+lines.forEach((line) => {
+  const [left] = line.split("  ").map(Number);
+  similarity += left * (frequencyMap[left] || 0);
+});
 
 log({ similarity });
