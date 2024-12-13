c5a783e61fd8ea1a0304418aa704f3c1d09d9598
rolcho <roland@cimem.hu>
2024-12-13 14:02:48 +0100

    d13-1 solved

--- 2024/day-13/day-13.ts
+++ 2024/day-13/day-13.ts
@@ -1,61 +1,68 @@
-/*
-Button A: X+94, Y+34
-Button B: X+22, Y+67
-Prize: X=8400, Y=5400
-a 10 b 20 d 100 s 0
-*/
-
 type Coordinate = { x: number; y: number };
 type Buttons = { a: Coordinate; b: Coordinate };
+type Machine = { buttons: Buttons; destination: Coordinate };
 
-// const buttons: Buttons = { a: { x: 10, y: 10 }, b: { x: 20, y: 20 } };
-// const destination = { x: 40, y: 40 };
-const buttons: Buttons = { a: { x: 94, y: 34 }, b: { x: 22, y: 67 } };
-const destination = { x: 8400, y: 5400 };
-const start: Coordinate = { x: 0, y: 0 };
-
-function getRouteSteps(
-  buttons: Buttons,
-  origin: Coordinate,
-  destination: Coordinate,
-  stepCounter: number
-): number | undefined {
-  //TODO: create deep equality helper function
-  if (origin.x === destination.x && origin.y === destination.y) {
-    return stepCounter;
+async function getFileContent(
+  contentType: "example" | "input"
+): Promise<string> {
+  let fileContent = "";
+  try {
+    const input = Bun.file(`${__dirname}/${contentType}.txt`);
+    fileContent = (await input.text()).trim();
+  } catch (error) {
+    console.error(error);
   }
-  if (origin.x > destination.x || origin.y > destination.y) {
-    return undefined;
-  }
-
-  const { a, b } = buttons;
-  const aInc = { x: origin.x + a.x, y: origin.y + a.y };
-  const bInc = { x: origin.x + b.x, y: origin.y + b.y };
-
-  const buttonPressA = getRouteSteps(
-    buttons,
-    aInc,
-    destination,
-    stepCounter + 1
-  );
-  const buttonPressB = getRouteSteps(
-    buttons,
-    bInc,
-    destination,
-    stepCounter + 1
-  );
-
-  if (!buttonPressA && !buttonPressB) {
-    return undefined;
-  }
-  if (!buttonPressA) {
-    return buttonPressB;
-  }
-  if (!buttonPressB) {
-    return buttonPressA;
-  }
-  return Math.min(buttonPressA, buttonPressB);
+  return fileContent;
 }
 
-const result = getRouteSteps(buttons, start, destination, 0);
-console.log({ result });
+function processInput(input: string): Machine[] {
+  const machines: Machine[] = [];
+  const machinesRaw = input.split("\n\n").map((machine) => machine.split("\n"));
+  for (const machine of machinesRaw) {
+    const buttonA = machine[0].slice(12).split(", Y+").map(Number);
+    const buttonB = machine[1].slice(12).split(", Y+").map(Number);
+    const destination = machine[2].slice(9).split(", Y=").map(Number);
+    machines.push({
+      buttons: {
+        a: { x: buttonA[0], y: buttonA[1] },
+        b: { x: buttonB[0], y: buttonB[1] },
+      },
+      destination: { x: destination[0], y: destination[1] },
+    });
+  }
+  return machines;
+}
+
+function getMinTokens(buttons: Buttons, destination: Coordinate): number {
+  const { a, b } = buttons;
+  const validTokenAmounts: number[] = [];
+
+  for (let i = 1; i < 100; i++) {
+    const remain = { x: destination.x - i * a.x, y: destination.y - i * a.y };
+    const isRemainXDividable = remain.x % b.x === 0;
+    const isRemainYDividable = remain.y % b.y === 0;
+    const remainX = remain.x / b.x;
+    const remainY = remain.y / b.y;
+    if (
+      isRemainXDividable &&
+      isRemainYDividable &&
+      remainX === remainY &&
+      remainX < 100
+    ) {
+      validTokenAmounts.push(i * 3 + remainX);
+    }
+  }
+  return validTokenAmounts.length === 0 ? 0 : Math.min(...validTokenAmounts);
+}
+
+async function main() {
+  const input = await getFileContent("input");
+  const machines = processInput(input);
+  let sum = 0;
+  for (const machine of machines) {
+    const { buttons, destination } = machine;
+    sum += getMinTokens(buttons, destination);
+  }
+  console.log(sum);
+}
+main();
