471057c7c0c406590a72f93a135cfab5b17f239d
rolcho <roland@cimem.hu>
2024-12-23 17:35:23 +0100

    d15 filter items

--- 2024/day-15/day-15.ts
+++ 2024/day-15/day-15.ts
@@ -1,9 +1,10 @@
+const ROBOT = "@";
+const BOX = "O";
+const WALL = "#";
 type Coordinate = { x: number; y: number };
-type Items = {
-  walls: Coordinate[];
-  boxes: Coordinate[];
-  robot: Coordinate;
-};
+type ItemType = "#" | "O" | "@";
+type Item = { item: ItemType } & Coordinate;
+type Items = Item[];
 async function getFileContent(
   contentType: "example" | "input"
 ): Promise<string> {
@@ -21,81 +22,38 @@ function preProcess(str: string): {
   items: Items;
   route: string[];
 } {
-  const walls: Coordinate[] = [];
-  const boxes: Coordinate[] = [];
-  const robot: Coordinate = { x: 0, y: 0 };
+  const items: Items = [];
   const [map, routeStr] = str.split("\n\n");
   const route = routeStr.split("");
   const data = map.split("\n").map((l) => l.split(""));
   for (let row = 0; row < data.length; row++) {
     for (let column = 0; column < data[0].length; column++) {
-      switch (data[row][column]) {
-        case "#":
-          walls.push({ x: column, y: row });
-          break;
-        case "O":
-          boxes.push({ x: column, y: row });
-          break;
-        case "@":
-          robot.x = column;
-          robot.y = row;
-          break;
-      }
+      if (
+        data[row][column] === "#" ||
+        data[row][column] === "O" ||
+        data[row][column] === "@"
+      )
+        items.push({ item: data[row][column] as ItemType, x: column, y: row });
     }
   }
-  const items = { walls, boxes, robot };
   return { items, route };
 }
 
-function moveItems(items: Items, route: string[]): Coordinate {
-  const { robot, boxes, walls } = items;
-  const directions = {
-    up: { x: 0, y: -1 },
-    down: { x: 1, y: 1 },
-    left: { x: -1, y: 0 },
-    right: { x: 1, y: 0 },
-  } as const;
-  const executeMove = (direction: Coordinate) => {
-    const closestWall = walls
-      .filter(
-        (w) => w.x === robot.x + direction.x && w.y === robot.y + direction.y
-      )
-      .sort(
-        (a, b) =>
-          Math.abs(robot.x - a.x) +
-          Math.abs(robot.y - a.y) -
-          Math.abs(robot.x - b.x) -
-          Math.abs(robot.y - b.y)
-      )[0];
-    if (
-      closestWall.x === robot.x + direction.x &&
-      closestWall.y === robot.y + direction.y
-    )
-      return;
-  };
-  while (route.length > 0) {
-    const nextMove = route.shift();
-    switch (nextMove) {
-      case "v":
-        executeMove(directions.down);
-        break;
-      case "^":
-        executeMove(directions.up);
-        break;
-      case "<":
-        executeMove(directions.left);
-        break;
-      case ">":
-        executeMove(directions.right);
-        break;
-    }
-  }
-  return {} as Coordinate;
+function getItemPositions(
+  item: ItemType,
+  items: Items
+): Coordinate | Coordinate[] {
+  const itemList = items.filter((i) => i.item === item);
+  if (itemList.length === 0) return [];
+  const coordinates: Coordinate[] = itemList.map((i) => ({ x: i.x, y: i.y }));
+  return coordinates;
 }
 async function main() {
   const fileContent = await getFileContent("example");
   const { items, route } = preProcess(fileContent);
-  console.log({ w: items.walls, b: items.boxes, r: items.robot, route });
-  const endPosition = moveItems(items, route);
+  console.log({ items, route });
+  console.log({ robot: getItemPositions(ROBOT, items)[0] });
+  console.log({ walls: getItemPositions(WALL, items) });
+  console.log({ boxes: getItemPositions(BOX, items) });
 }
 main();
